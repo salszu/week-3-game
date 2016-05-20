2e608ebc65271cda954e5915eeb8821a5153f5e1
Salwador Szurlej <sal.szu@gmail.com>
2016-05-20 19:16:21 -0400

    starting javascripts

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+// possible choices
+var letters = 0;
+var wins = 0;
+var losses = 0;	
+var possibleChoices = ['a', 'b', 'c', 'd', 'e',
+'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var redX = 0;
+
+$(document).ready(function(){
+	// ready debug
+	console.log("READY!");
+
+	// possible choices
+	var letters = 0;
+	var wins = 0;
+	var losses = 0;	
+	var possibleChoices = ['a', 'b', 'c', 'd', 'e',
+	'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+	var redX = 0;
+	//resetting wins/losses
+	document.getElementById('wins').innerHTML = '0';
+	document.getElementById('losses').innerHTML=  '0';
+
+
+})
