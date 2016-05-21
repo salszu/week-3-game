771695611f06677c5385ea7de9ddf2467e447360
Salwador Szurlej <sal.szu@gmail.com>
2016-05-20 20:49:09 -0400

    lots of ideas, little execution

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,63 @@
-// possible choices
-var letters = 0;
-var wins = 0;
-var losses = 0;	
-var possibleChoices = ['a', 'b', 'c', 'd', 'e',
-'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var redX = 0;
 
 $(document).ready(function(){
 	// ready debug
 	console.log("READY!");
 
-	// possible choices
+	// variables
 	var letters = 0;
 	var wins = 0;
 	var losses = 0;	
+	var word = ['b', 'a', 'r', 'l', 'e', 'y'];
 	var possibleChoices = ['a', 'b', 'c', 'd', 'e',
 	'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 	var redX = 0;
-	//resetting wins/losses
+	//var usedLetter = [possibleChoices - ]
+	//resetting wins/losses/guesses
 	document.getElementById('wins').innerHTML = '0';
 	document.getElementById('losses').innerHTML=  '0';
+	document.getElementById('guessesLeft').innerHTML= "6";
+
+		//randomizer for choosing a word to start with 
+    	var choice = possibleChoices[Math.floor(Math.random() * possibleChoices.length)];	
+
+		//onkeyup 
+		document.getElementById("letters").onkeyup = function(){
+
+			var keyPress = document.getElementById("letters");
+
+		//onkeyUP#2?
+			document.onkeyup = function(event) {
+				var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+			}
+
+		};	
 
 
-})
+		// creating underscores for the chosen words for loop
+		for( var i = 0; i <= words.length; i++) {
+			
+
+
+			//document.getElementById('uScores').innerHTML= ;
+		}
+		
+
+		//use css to create the underscores you fucking jackass  border-bottom: 1px solid black;
+
+		
+
+		//
+
+    		
+		// need to code a function for when possiblechoices =/= wordX, redX becomes visible 
+		// another one becomes visible
+
+		
+    	
+
+
+
+
+
+
+
